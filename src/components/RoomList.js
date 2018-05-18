7cb33f52a493d78f861f5455cda475c0a3e26076
Erin Comerford <erin.comerford3@gmail.com>
2018-05-17 17:01:38 -0700

    Create New Room checkpoint complete.

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -7,27 +7,61 @@ class RoomList extends Component {
       rooms: []
     }
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.handleChange = this.handleChange.bind(this);
+    this.createRoom = this.createRoom.bind(this);
   }
 
 
   componentDidMount(){
-      this.roomsRef.on("child_added", snapshot => {
-          const room = snapshot.val();
-          room.key = snapshot.key;
-          this.setState({ rooms: this.state.rooms.concat( room ) })
-          console.log(this.state.rooms)
-      })
+    this.roomsRef.on("child_added", snapshot => {
+        const room = snapshot.val();
+        room.key = snapshot.key;
+        this.setState({ rooms: this.state.rooms.concat( room ) })
+    })
+  }
+
+  handleChange(event) {
+    this.setState({name: event.target.value})
+  }
+
+  createRoom=(e) => {
+    e.preventDefault();
+    this.roomsRef.push({
+      name: this.state.name
+    });
   }
 
   render() {
     return (
-      <div className="rooms">
-        {
-          this.state.rooms.map((data, index) =>
-            <p key={index}>{data.name}</p>
-          )
-        }
-      </div>
+      <section className="rooms">
+        <div className="list">
+          {
+            this.state.rooms.map((data, index) =>
+              <p key={index}>{data.name}</p>
+            )
+          }
+        </div>
+        <form
+          className="newRoomForm"
+          onSubmit={this.createRoom}
+        >
+          <label>
+            Create a New Room:
+            <input
+              type="text"
+              className="room-name"
+              name="create-room"
+              placeholder="Enter New Room Name."
+              value={this.state.rooms.name}
+              onChange={this.handleChange}
+            / >
+          </label>
+          <input
+            type="submit"
+            value="Submit"
+          / >
+        </form>
+      </section>
     )
   }
 }
